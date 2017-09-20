7d5981aa02b20316fbc5a863d7b4a2f4b716a36f
Keesh zhang <chunhui2001@gmail.com>
2017-09-20 14:20:07 +0800

    init supercard project with multiple module

---
+++ supercard/supercard-parseremail/src/main/java/com/supercard/parseremail/App.java
@@ -0,0 +1,165 @@
+package com.supercard.parseremail;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.supercard.parseremail.cardparse.*;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.mail.util.MimeMessageParser;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Properties;
+
+import javax.mail.*;
+import javax.mail.internet.MimeMessage;
+import javax.mail.search.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+
+    public static String readFile(String filePath)  {
+
+        try (FileInputStream inputStream = new FileInputStream(filePath)) {
+            return IOUtils.toString(inputStream, "utf-8");
+        } catch (IOException e) {
+            return null;
+        }
+
+    }
+
+    public static void main( String[] args ) throws Exception {
+
+
+        String host = "pop.qq.com";
+        String email = "76920104@qq.com";
+        String password = "ujwljrfbjdydbigc";
+
+        if (1==2) {
+
+//            ParsePCCCEmail parsePCCCEmail = new ParsePCCCEmail(email, readFile("/Users/keesh/Desktop/demo.html"));
+//            Collection<BillEntity> billEntityList = parsePCCCEmail.parse();
+//            System.out.println(new ObjectMapper().writeValueAsString(billEntityList));
+
+//
+//            ParseCmbEmail parsePmbEmail = new ParseCmbEmail(email, readFile("/Users/keesh/Desktop/demo.html"));
+//            Collection<BillEntity> billEntityList = parsePmbEmail.parse();
+//            System.out.println(new ObjectMapper().writeValueAsString(billEntityList));
+
+//            ParseCiticEmail parseCiticEmail = new ParseCiticEmail(email, readFile("/Users/keesh/Desktop/demo.html"));
+//            Collection<BillEntity> billEntityList = parseCiticEmail.parse();
+//            System.out.println(new ObjectMapper().writeValueAsString(billEntityList));
+
+
+//            ParseSpdbEmail parseSpdbEmail = new ParseSpdbEmail(email, readFile("/Users/keesh/Desktop/demo.html"));
+//            Collection<BillEntity> billEntityList = parseSpdbEmail.parse();
+//            System.out.println(new ObjectMapper().writeValueAsString(billEntityList));
+
+//            new ParseCitiBankEmail(email, null).parse();
+
+            new ParseCgbEmail(email, null).parse();
+
+            return;
+        }
+
+        Properties props = new Properties() {{
+            setProperty("mail.pop3.host", "pop.qq.com");
+            setProperty("mail.pop3.port", "995");
+        }};
+
+        // SSL安全连接参数
+        props.setProperty("mail.pop3.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.setProperty("mail.pop3.socketFactory.fallback", "true");
+        props.setProperty("mail.pop3.socketFactory.port", "995");
+
+        Session session = Session.getDefaultInstance(props, null);
+        Store store = session.getStore("pop3");
+        store.connect(host, email, password);
+
+        Folder folder = store.getFolder("INBOX");
+        folder.open(Folder.READ_ONLY);
+
+        // Message message[] = folder.getMessages();
+        Message message[] = folder.search(new AndTerm(
+                new OrTerm(
+                    new FromStringTerm[]{
+                        new FromStringTerm("广发银行"),     // 补发的账单是以PDF附件形式发送的
+                        new FromStringTerm("中信银行"),
+                        new FromStringTerm("交通银行"),
+                        new FromStringTerm("招商银行"),           // 有的对账单在邮件里面只有到期还款日和还款金额，需要在网页内登陆查看账单详细信息
+                        new FromStringTerm("浦发银行")  // 邮件内是一个链接地址
+                }),
+                new OrTerm(
+                    new SubjectTerm[]{
+                        new SubjectTerm("账单")
+                })));
+
+        System.out.println("邮件数量:　" + message.length);
+
+        MimeMessageParser parser = null;
+
+        for (int i = 0; i < message.length; i++) {
+
+            MimeMessage mimeMessage = (MimeMessage) message[i];
+            parser = new MimeMessageParser(mimeMessage).parse();
+
+            String from = parser.getFrom();
+
+            Collection<BillEntity> billEntityList = null;
+
+            System.out.println("");
+
+            try {
+
+                switch (from) {
+                    case "PCCC@bocomcc.com":
+                        System.out.println("============== " + from + " [交通 FOCUS][" + (i + 1) + "] ==============");
+                        billEntityList = new ParsePCCCEmail(email, mimeMessage, parser).parse();
+                        break;
+                    case "citiccard@citiccard.com":
+                        System.out.println("============== " + from + " [中信 FOCUS][" + (i + 1) + "] ==============");
+                        billEntityList = new ParseCiticEmail(email, mimeMessage, parser).parse();
+                        break;
+                    case "estmtservice@eb.spdbccc.com.cn":
+                        System.out.println("============== " + from + " [浦发 FOCUS][" + (i + 1) + "] ==============");
+                        billEntityList = new ParseSpdbEmail(email, mimeMessage, parser).parse();
+                        break;
+                    case "creditcard@cgbchina.com.cn":
+                        System.out.println("============== " + from + " [广发 FOCUS][" + (i + 1) + "] ==============");
+                        billEntityList = new ParseCgbEmail(email, mimeMessage, parser).parse();
+                        break;
+                    case "ccsvc@message.cmbchina.com":
+                        System.out.println("============== " + from + " [招商 FOCUS][" + (i + 1) + "] ==============");
+                        billEntityList = new ParseCmbEmail(email, mimeMessage, parser).parse();
+                        break;
+                    case "citibank@emailmgmt.china.citibank.com":
+                        System.out.println("============== " + from + " [花旗 FOCUS][" + (i + 1) + "] ==============");
+                        billEntityList = new ParseCitiBankEmail(email, mimeMessage, parser).parse();
+                        break;
+                    default:
+                        System.out.println(from + " [IGNORE][" + (i + 1) + "]");
+                        continue;
+
+                }
+
+            } catch (Exception e) {
+                    System.out.print(parser.getHtmlContent());
+                System.out.print(from + "ERROR");
+            }
+
+            System.out.println(new ObjectMapper().writeValueAsString(billEntityList));
+            System.out.println("============== *************** ==============");
+
+        }
+
+        folder.close(true);
+        store.close();
+
+        System.out.println("");
+        System.out.println("DONE! Parse email bills completed! DONE!");
+
+    }
+}
---
+++ supercard/supercard-parseremail/src/main/java/com/supercard/parseremail/cardparse/ParseSpdbEmail.java
@@ -0,0 +1,139 @@
+package com.supercard.parseremail.cardparse;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.supercard.parseremail.BillEntity;
+import com.supercard.parseremail.entities.BillItem;
+import org.apache.commons.mail.util.MimeMessageParser;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+
+import javax.mail.internet.MimeMessage;
+import java.io.IOException;
+import java.util.*;
+
+/**
+ * Created by keesh on 11/09/2017.
+ */
+public class ParseSpdbEmail extends ParseEmailBase {
+
+    private static final String homeDataAction = "https://ebill.spdbccc.com.cn/cloudbank-portal/myBillController/loadHomeData.action";
+
+    private String cookieString = null;
+
+    public ParseSpdbEmail() {
+
+    }
+
+    public ParseSpdbEmail(String userEmail, String htmlContent) {
+        this.useremail = userEmail;
+        this.content = htmlContent;
+        this.emailHtmlDoc = Jsoup.parse(this.content);
+    }
+
+    public ParseSpdbEmail(String userEmail, MimeMessage message, MimeMessageParser parser) throws Exception {
+        super(userEmail, message, parser);
+    }
+
+    public String getBillJson() {
+
+        Element element = this.emailHtmlDoc.select("a").first();
+        String billUrl = escapeContent(element.attr("href")).replaceAll(" ", "");
+
+        this.cookieString = doGet(billUrl, null, null);
+
+        if (this.cookieString == null) return null;
+
+        return doPost(homeDataAction, this.cookieString, null);
+
+    }
+
+    @Override
+    public Collection<BillEntity> parse() {
+
+        String billJsonString = getBillJson();
+
+        Map<String,String> billMap = null;
+
+        try {
+            billMap = new ObjectMapper().readValue(billJsonString, new TypeReference<HashMap<String,String>>(){});
+        } catch (IOException e) {
+            return null;
+        }
+
+        if (billMap == null) return null;
+
+        BillEntity billEntity = new BillEntity();
+
+        billEntity.setUserIdentity(this.useremail);
+        billEntity.setBillMoney(billMap.get("cash"));
+        billEntity.setBillMoneyMin(billMap.get("minPay"));
+        billEntity.setBillExpired(billMap.get("lastBackDate"));
+        billEntity.setBillMonth(billMap.get("billYM"));
+        billEntity.setCustomerName(billMap.get("userName").split(" ")[0]);
+        billEntity.setGender(billMap.get("userName").split(" ")[1]);
+        billEntity.setCardNumber(billMap.get("cardNo"));
+
+
+        billEntity.setBillItems(parseBillItems());
+
+        return new ArrayList<BillEntity>() {{ add(billEntity); }};
+
+    }
+
+    private Collection<BillItem> parseBillItems() {
+
+        String billItemAction = "https://ebill.spdbccc.com.cn/cloudbank-portal/billDetailController/PCloadBillsDetailAll.action";
+
+        String billItemResultString = doPost(billItemAction, this.cookieString, null);
+
+        if (billItemResultString == null) return null;
+
+        Map<String, Object> result = new HashMap<>();
+
+        try {
+            result = new ObjectMapper().readValue(billItemResultString, new TypeReference<HashMap<String,Object>>(){});
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        List<LinkedHashMap<String, String>> itemList =  (ArrayList<LinkedHashMap<String, String>>)result.get("list");
+        Collection<BillItem> itemListEntity = new ArrayList<>();
+        BillItem billItemEntity = null;
+
+
+        for (int i =0; i<itemList.size(); i++) {
+
+            billItemEntity = new BillItem();
+
+            // amountFL: D consumption, C repayment
+
+            billItemEntity.setTransMoney(itemList.get(i).get("amount"));
+            billItemEntity.setTransDesc(itemList.get(i).get("desLine1") + " " + itemList.get(i).get("desLine2"));
+            billItemEntity.setTransCurrency("RMB");
+            billItemEntity.setTransCardNumber(itemList.get(i).get("cardNbr1"));
+            billItemEntity.setTransDate(itemList.get(i).get("purDate8"));
+            billItemEntity.setRecordDate(null);
+            billItemEntity.setTransArea(null);
+
+            if (itemList.get(i).get("amountFl").endsWith("D")) {
+                billItemEntity.setTransType("consumption");
+            } else if (itemList.get(i).get("amountFl").endsWith("C")) {
+                billItemEntity.setTransType("repayment");
+            } else {
+                billItemEntity.setTransType(itemList.get(i).get("amountFl"));
+            }
+
+            itemListEntity.add(billItemEntity);
+
+        }
+
+
+        return itemListEntity;
+
+    }
+
+
+
+
+}
