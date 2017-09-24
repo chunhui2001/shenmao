5cf96cde2664b98663e6e8bc762d3886fbbab211
Keesh <Chunhui2001@gmail.com>
2017-09-24 21:37:40 +0800

    implement callable and future demo

---
+++ supercard/supercard-lab/src/main/java/com/supercard/lab/thread/callablefuture/CallableFutureDemo.java
@@ -0,0 +1,75 @@
+package com.supercard.lab.thread.callablefuture;
+
+import java.io.IOException;
+import java.util.Random;
+import java.util.concurrent.*;
+
+public class CallableFutureDemo {
+
+    public static void main(String[] args) {
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        executorService.submit(new Runnable() {
+
+            @Override
+            public void run() {
+
+                Random random = new Random();
+
+                Integer duration = random.nextInt(4000);
+
+                System.out.println("Starting Runnable Thread ...");
+
+                System.out.println("Runnable Thread sleeping: " + duration);
+
+                try {
+                    Thread.sleep(duration);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                System.out.println("Finished Runnable Thread ...");
+
+            }
+
+        });
+
+        Future<Integer> futureResult = executorService.submit(new Callable<Integer>() {
+
+            @Override
+            public Integer call() throws Exception {
+
+                Random random = new Random();
+
+                Integer duration = random.nextInt(4000);
+
+                if (duration > 2000) {
+                    throw new IllegalArgumentException("Sleeping duration is too long: " + duration);
+                }
+
+                try {
+                    Thread.sleep(duration);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                return duration;
+
+            }
+
+        });
+
+        executorService.shutdown();
+
+        try {
+            System.out.println("The future result is: " + futureResult.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
